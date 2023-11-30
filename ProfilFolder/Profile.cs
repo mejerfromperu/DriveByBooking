f0efa1b8df4d4b1e8cb89338160a4f56bdf38d91
Christian Alexander Yábar Mejer <chm003@edu.zealand.dk>
2023-11-30 09:55:42 +0100

    profile og arv

---
+++ ProfilFolder/Profile.cs
@@ -0,0 +1,53 @@
+﻿namespace DriveByBooking.ProfilFolder
+{
+    public class Profile
+    {
+
+        //
+        // Instance field...
+        //
+        protected string _username;
+        protected string _password;
+        protected string _name;
+
+        //
+        // Properties...
+        //
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        //
+        // Constructor...
+        // Default
+        public Profile()
+        {
+            _username = string.Empty;
+            _password = string.Empty;
+            _name = string.Empty;
+        }
+        // Constructor
+        public Profile(string username, string password, string name)
+        {
+            _username = username;
+            _password = password;
+            _name = name;
+        }
+
+        //
+        // Methods...
+        //
+    }
+}
