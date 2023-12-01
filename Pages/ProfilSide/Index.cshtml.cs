d01c3b88a9e02b257d345fc0b81d75a21570be50
mejerfromperu <christianmejer7@gmail.com>
2023-12-01 10:41:48 +0100

    Admin side Profil side

---
+++ Pages/ProfilSide/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DriveByBooking.Pages.ProfilSide
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
