3f1883cf918bbcdb4ffa45f40ba201d02fec2e7b
Isak Gaardsted Møller <ism001@edu.zealand.dk>
2023-12-01 10:23:30 +0100

    Booking

---
+++ Model/Booking/Booking.cs
@@ -0,0 +1,40 @@
+﻿using DriveByBooking.Model.CarFolder;
+using DriveByBooking.Model.ProfilFolder;
+
+namespace DriveByBooking.Model.Booking
+{
+    public class Booking
+    {
+
+        private CarClass _car;
+        private CustomerClass _customer;
+
+
+        public CarClass Car
+        {
+            get { return _car; }
+            set { _car = value; }
+        }
+
+        public CustomerClass CustomerClass 
+        { 
+            get { return _customer; }
+            set {  _customer = value; } 
+        }
+
+        public Booking()
+        {
+            _car = null;
+            _customer = null;
+        }
+
+        public Booking (CarClass car, CustomerClass customer)
+        {
+            _car = car;
+            _customer = customer;
+            
+        }
+
+
+    }
+}
