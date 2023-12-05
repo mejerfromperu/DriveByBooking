7d6f657125b7fdebaece96f75471d75a69010133
Jonas Knygberg Kristiansen <jok003@edu.zealand.dk>
2023-12-05 09:48:54 +0100

    fofofssets

---
+++ Pages/Garage1/Index.cshtml.cs
@@ -0,0 +1,49 @@
+using DriveByBooking.Model.CarFolder;
+using DriveByBooking.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DriveByBooking.Pages.Garage1
+{
+    public class IndexModel : PageModel
+    {
+        private ICarRepository _list;
+
+        public IndexModel(ICarRepository list)
+        {
+            _list = list;
+        }
+
+        public List<CarClass> Cars { get; set; }
+
+
+        [BindProperty]
+        public string? SearchName { get; set; }
+
+        [BindProperty]
+        public double? SearchPrice { get; set; }
+
+        [BindProperty]
+        public string? SearchBrand { get; set; }
+
+        public void OnGet()
+        {
+            Cars = new List<CarClass>();
+        }
+
+        public RedirectToPageResult OnPost()
+        {
+            return RedirectToPage("Index");
+        }
+
+        public IActionResult OnPostSearch()
+        {
+            Cars = _list.Search(SearchName, SearchPrice, SearchBrand);
+            return Page();
+        }
+
+
+
+
+    }
+}
